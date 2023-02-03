5081e4fe200562c9eba3400a2068695edbee00c1
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2023-02-03 15:03:47 -0800

    raw script worked out

--- pymodel_rotate.py
+++ pymodel_rotate.py
@@ -17,14 +17,14 @@ r_out = 0.3
 r_in = 0.2
 width = 0.1
 spoke_width = 0.04
-num_spokes = 4
+num_spokes = 5
 meshsize = 0.02
 r_depth = 0.02
 r_pressure = 0.1
 E = 1e8
 mu = 0.3
 load = 10000
-init_angle = 0
+init_angle = 20
 results_location = 'C:/Users/bowen/Desktop/'
 
 # Names
@@ -39,9 +39,11 @@ job_name = 'wheel_compression'
 
 # Derived values
 search_point_whole = (0.0, r_out, width / 2)
-search_point_lateral = (0.0, r_out, width / 2)
+search_point_load = (0.0, r_out, width / 2)
+search_point_bc = (0.0, -r_out, width / 2)
 search_point_extrusion = (0.0, (r_in + r_out) / 2, width)
 search_point_outer_edge = (0.0, r_out, width)
+search_point_mesh_edge = (r_out, 0.0, width / 2)
 
 spoke_start = (r_out + r_in) / 2
 search_points_spoke = [(-spoke_start + 0.01, spoke_width / 2),
@@ -72,7 +74,7 @@ for i in range(num_spokes):
     mysketch = mymodel.sketches['__profile__']
     mypart.projectReferencesOntoSketch(filter=COPLANAR_EDGES, sketch=mysketch)
     mysketch.rectangle(point1=(-spoke_start, -spoke_width / 2), point2=(spoke_start, spoke_width / 2))
-    mysketch.rotate(angle=init_angle+rotate_angle*(i), centerPoint=(0.0, 0.0),
+    mysketch.rotate(angle=init_angle + rotate_angle * (i), centerPoint=(0.0, 0.0),
                     objectList=(
                         mysketch.geometry.findAt(search_points_spoke[0], ),
                         mysketch.geometry.findAt(search_points_spoke[1], ),
@@ -87,7 +89,7 @@ mypart.Set(faces=mypart.faces.getByBoundingSphere(center=(0, 0, 0), radius=10.0)
 
 # Material & Section
 mymodel.Material(name=material_name)
-mymodel.materials[material_name].Elastic(table=((E, mu), ))
+mymodel.materials[material_name].Elastic(table=((E, mu),))
 mymodel.HomogeneousSolidSection(material=material_name, name=section_name, thickness=None)
 mypart.SectionAssignment(offset=0.0, offsetField='', offsetType=MIDDLE_SURFACE,
                          region=Region(cells=mypart.cells.findAt((search_point_whole,), )),
@@ -102,32 +104,45 @@ myassembly = mymodel.rootAssembly.instances[assembly_name]
 mymodel.StaticStep(name=step_name, previous='Initial')
 
 # Mesh
-mypart.seedPart(deviationFactor=0.1, minSizeFactor=0.1, size=meshsize)
+
+
 mypart.setMeshControls(elemShape=TET, regions=mypart.cells.findAt((search_point_whole,), ), technique=FREE)
 mypart.setElementType(elemTypes=(ElemType(elemCode=C3D8R, elemLibrary=STANDARD),
                                  ElemType(elemCode=C3D6, elemLibrary=STANDARD),
                                  ElemType(elemCode=C3D4, elemLibrary=STANDARD,
                                           secondOrderAccuracy=OFF, distortionControl=DEFAULT)),
                       regions=(mypart.cells.findAt(((0.0, r_out, width / 2),), ),))
+mypart.PartitionCellByPlaneThreePoints(cells=mypart.cells.findAt((search_point_whole,), ),
+                                       point1=(0, 0, 0), point2=(1, 0, 0), point3=(1, 0, 1))
+# mypart.Set(edges=mypart.faces.findAt(((search_point_mesh_edge,),)),
+#            name='all_faces')
+mypart.seedEdgeBySize(constraint=FINER, deviationFactor=0.1,
+                      edges=mypart.edges.findAt((search_point_mesh_edge,), ), size=0.01)
+mypart.seedPart(deviationFactor=0.1, minSizeFactor=0.1, size=meshsize)
 mypart.generateMesh()
 
 # get nodes for loading and BC
-mypart.Set(faces=mypart.faces.findAt((search_point_lateral,), ), name='face_big')
-face_big = mypart.sets['face_big'].faces[0]
-mypart.Set(nodes=face_big.getNodes(), name='face_nodes')
-face_big_nodes = mypart.sets['face_nodes'].nodes
-mypart.Set(nodes=face_big_nodes.getByBoundingCylinder(center1=(0.0, r_out - r_depth, width / 2),
-                                                      center2=(0.0, r_out + r_depth, width / 2),
-                                                      radius=r_pressure), name='nodes_load')
+mypart.Set(faces=mypart.faces.findAt((search_point_load,), ), name='face_load')
+face_load = mypart.sets['face_load'].faces[0]
+mypart.Set(nodes=face_load.getNodes(), name='face_load_nodes')
+face_load_nodes = mypart.sets['face_load_nodes'].nodes
+mypart.Set(nodes=face_load_nodes.getByBoundingCylinder(center1=(0.0, r_out - r_depth, width / 2),
+                                                       center2=(0.0, r_out + r_depth, width / 2),
+                                                       radius=r_pressure), name='nodes_load')
 nodes_load = mypart.sets['nodes_load'].nodes
-mypart.Set(nodes=face_big_nodes.getByBoundingCylinder(center1=(0.0, -(r_out - r_depth), width / 2),
-                                                      center2=(0.0, -(r_out + r_depth), width / 2),
-                                                      radius=r_pressure), name='nodes_bc')
+
+mypart.Set(faces=mypart.faces.findAt((search_point_bc,), ), name='face_bc')
+face_bc = mypart.sets['face_bc'].faces[0]
+mypart.Set(nodes=face_bc.getNodes(), name='face_bc_nodes')
+face_bc_nodes = mypart.sets['face_bc_nodes'].nodes
+mypart.Set(nodes=face_bc_nodes.getByBoundingCylinder(center1=(0.0, -(r_out - r_depth), width / 2),
+                                                     center2=(0.0, -(r_out + r_depth), width / 2),
+                                                     radius=r_pressure), name='nodes_bc')
 nodes_bc = mypart.sets['nodes_bc'].nodes
 
 # Load & BC
 num_nodes_load = len(nodes_load)
-mymodel.ConcentratedForce(cf2=-load/num_nodes_load, createStepName=step_name,
+mymodel.ConcentratedForce(cf2=-load / num_nodes_load, createStepName=step_name,
                           distributionType=UNIFORM, field='', localCsys=None, name=load_name,
                           region=myassembly.sets['nodes_load'])
 mymodel.EncastreBC(createStepName=step_name, localCsys=None, name=bc_name, region=myassembly.sets['nodes_bc'])
